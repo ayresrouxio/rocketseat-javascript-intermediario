f334374db5749c02e8aafacfcf5ae0a0bb5e76fa
Ayres Roux <ayresrouxio@gmail.com>
2024-12-16 06:54:54 -0300

    aula: acessando propriedades no contexto do objeto

--- scripts.js
+++ scripts.js
@@ -1,44 +1,10 @@
-/*
-  Objeto:
-  - Uma coleção de dados e/ou funcionalidades;
-  - Podem ter propriedades e métodos;
-*/
-
-// Cria um objeto vazio.
-const obj = {}
-
-console.log(obj)
-console.log(typeof obj)
-
-// Cria um objeto com propriedades e métodos.
 const user = {
+  name: "Rodrigo",
   email: "rodrigo@email.com",
-  age: 18,
-  name: {
-    first_name: "Rodrigo",
-    surname: "Gonçalves",
-  },
-  address: {
-    street: "Rua X",
-    number: 23,
-    city: "São Paulo",
-    postal_code: "123456-123",
-  },
-  message: () => {
-    console.log("Oi Rodrigo")
+  message: function () {
+    //console.log(`Olá ${user.name}`)
+    console.log(`Olá ${this.name}`)
   },
 }
 
-// Acessando propriedades e métodos usando notação de ponto.
-console.log(user.email)
-
-// Acessando propriedade de objetos.
-console.log(user.name.first_name)
-
-// Executa o método do objeto.
 user.message()
-
-// Notação de colchetes.
-console.log(user["email"])
-console.log(user["name"]["first_name"])
-user["message"]()
