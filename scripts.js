aa0c258a5754439304cf87861228404e8e31dedf
Ayres Roux <ayresrouxio@gmail.com>
2024-12-16 06:59:02 -0300

    aula: atualizando um objeto

--- scripts.js
+++ scripts.js
@@ -1,10 +1,22 @@
-const user = {
-  name: "Rodrigo",
-  email: "rodrigo@email.com",
-  message: function () {
-    //console.log(`Olá ${user.name}`)
-    console.log(`Olá ${this.name}`)
-  },
+const product = {
+  name: "Teclado",
+  quantity: 100,
 }
 
-user.message()
+// Acessando a propriedade do objeto.
+console.log(product.name)
+
+// Atualizar o valor de uma propriedade.
+console.log(product.quantity)
+
+product.quantity = 90
+
+console.log(product.quantity)
+
+console.log(product.name)
+product.name = "Mouse"
+console.log(product.name)
+
+// Notação de colchetes.
+product["quantity"] = 50
+console.log(product)
