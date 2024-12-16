b3b4606a370242537e19a617a5ba6219610ca1d3
Ayres Roux <ayresrouxio@gmail.com>
2024-12-16 07:33:17 -0300

    aula: o que é uma função construtora

--- scripts.js
+++ scripts.js
@@ -1,16 +1,44 @@
-/*
-  Operador de coalescência nula (??)
-  Operador lógico que retorna o seu operando do
-  lado direito quando o seu operador do lado
-  esquerdo é null ou undefined. Caso contrário,
-  ele retorna o seu operando do lado esquerdo.
-*/
-let content = undefined
-console.log(content ?? "Conteúdo padrão")
+function createProduct(name) {
+  const product = {}
 
-const user = {
-  name: "Rodrigo",
-  avatar: undefined,
+  product.name = name
+  product.details = function () {
+    console.log(`O nome do produto é ${this.name}`)
+  }
+
+  return product
 }
 
-console.log(user.avatar ?? "default.png")
+// O new cria um novo objeto utilizando a estrutura da função construtora.
+const product1 = new createProduct("Teclado")
+
+console.log(product1.name)
+product1.details()
+
+const product2 = new createProduct("Mouse")
+
+console.log(product2.name)
+product2.details()
+
+// console.log(product1 === product2)
+
+// Exemplos de funções construtoras disponíveis no próprio Javascript.
+let myName = new String("Rodrigo")
+console.log(myName)
+
+let price = "40.6".replace(".", "")
+console.log(price)
+
+let date = new Date("2024-1-1")
+console.log(date)
+
+function Person(name) {
+  this.name = name
+  this.message = function () {
+    console.log(`Olá, ${this.name}`)
+  }
+}
+
+const person1 = new Person("João")
+console.log(person1)
+person1.message()
