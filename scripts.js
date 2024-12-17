32221fbcaab757cdd58445e2f6824dcf271fde02
Ayres Roux <ayresrouxio@gmail.com>
2024-12-17 17:34:35 -0300

    aula: adicionando métodos nas classes

--- scripts.js
+++ scripts.js
@@ -1,11 +1,13 @@
-class Product {
-  constructor(name) {
+class User {
+  constructor(name, email) {
     this.name = name
+    this.email = email
+  }
+
+  sendEmail() {
+    console.log("E-mail enviado para:", this.name, this.email)
   }
 }
 
-const product1 = new Product("Teclado")
-console.log(product1.name)
-
-const product2 = new Product("Mouse")
-console.log(product2.name)
+const user = new User("Rodrigo", "rodrigo@email.com")
+user.sendEmail()
