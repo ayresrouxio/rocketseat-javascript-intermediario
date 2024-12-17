078031947176cbf634fcefacfa245e16bdeda1cb
Ayres Roux <ayresrouxio@gmail.com>
2024-12-17 07:14:15 -0300

    aula: métodos para trabalhar com data e hora

--- scripts.js
+++ scripts.js
@@ -1,10 +1,22 @@
-// Define com ano, mês (0 - 11), dia.
-console.log(new Date(2024, 6, 3))
+let date = new Date("2024-07-02T14:30:10")
 
-// Define data (ano, mês e dia) e hora (hora, minuto e segundos).
-console.log(new Date(2024, 6, 3, 14, 30, 0))
+// Dia da semana de 0 à 6 (domingo é 0).
+console.log(date.getDay())
 
-// Definindo data e hora com String.
-console.log(new Date("2024-07-03T14:30:00"))
+// Dia do mês de 0 à 30.
+console.log(date.getDate())
 
-console.log(new Date("July 3, 2024 14:30:00"))
+// Mês (0 à 11).
+console.log(date.getMonth())
+
+// Ano.
+console.log(date.getFullYear())
+
+// Horas.
+console.log(date.getHours())
+
+// Minutos.
+console.log(date.getMinutes())
+
+// Segundos.
+console.log(date.getSeconds())
