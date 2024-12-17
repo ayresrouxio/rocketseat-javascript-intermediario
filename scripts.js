8378b23171ac5996b4821e5d4e212279c1a1a46c
Ayres Roux <ayresrouxio@gmail.com>
2024-12-16 23:18:34 -0300

    aula loop do while

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
-// Loop infinito
-let value = true
+// Do While: repete até que a condição especificada seja verdadeira. No entanto, a instrução será executada pelo menos uma vez antes da condição ser verificada.
 
-/*
-while (value) {
-  console.log("Executando while...");
-}
-*/
+let value = 0
+
+do {
+  value++
+  console.log(value)
+} while (value < 10)
+
+console.log("Segue fluxo...")
