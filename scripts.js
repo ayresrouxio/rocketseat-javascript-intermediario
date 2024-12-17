a8cc82db92b60e4c9714249a53534e32ff9ef605
Ayres Roux <ayresrouxio@gmail.com>
2024-12-17 07:08:59 -0300

    aula: definindo uma data e uma hora especifica

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
-// Exibe a data e hora atual.
-console.log(new Date())
+// Define com ano, mês (0 - 11), dia.
+console.log(new Date(2024, 6, 3))
 
-// Exibe a data e hora de referência (considera o fuso).
-console.log(new Date(0))
+// Define data (ano, mês e dia) e hora (hora, minuto e segundos).
+console.log(new Date(2024, 6, 3, 14, 30, 0))
 
-// Exibe o número de milissegundos.
-console.log(new Date().getTime())
+// Definindo data e hora com String.
+console.log(new Date("2024-07-03T14:30:00"))
+
+console.log(new Date("July 3, 2024 14:30:00"))
