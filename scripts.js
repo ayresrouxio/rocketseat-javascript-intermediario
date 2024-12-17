5cd3777df5ae99efd91f1b222c5b357ceaca716b
Ayres Roux <ayresrouxio@gmail.com>
2024-12-17 06:50:19 -0300

    aula: break

--- scripts.js
+++ scripts.js
@@ -1,6 +1,27 @@
-// for ... of itera sobre valores de um objeto iterável.
-let students = ["Rodrigo", "Amanda", "John"]
+// Break encerra a execução da repetição ou switch para seguir para próxima instrução.
 
-for (let student of students) {
-  console.log(student)
+let option = 3
+
+switch (option) {
+  case 1:
+    console.log("Cadastrar")
+    break
+  case 2:
+    console.log("Editar")
+    break
+  case 3:
+    console.log("Excluir")
+    break
+  default:
+    console.log("Opção inválida")
+    break
+}
+
+// Utilizando o break para finalizar a repetição.
+for (let i = 0; i < 10; i++) {
+  if (i === 5) {
+    break
+  }
+
+  console.log(i)
 }
