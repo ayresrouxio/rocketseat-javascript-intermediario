5961acaf5bd317bc0057511d81bb9f399453f70e
Ayres Roux <ayresrouxio@gmail.com>
2024-12-17 07:44:47 -0300

    aula: usando o toLocaleString()

--- scripts.js
+++ scripts.js
@@ -1,11 +1,45 @@
-let date = new Date("2024-07-02T14:30:15")
+let date = new Date("2024-07-02T14:30:10")
 
-// Exibe a data e hora no formato local.
-console.log(date.toLocaleDateString())
-console.log(date.toLocaleTimeString())
 console.log(date.toLocaleString())
 
-// Exibe a data e hora no formato escolhido.
-console.log(date.toLocaleDateString("pt-BR"))
-console.log(date.toLocaleTimeString("pt-BR"))
-console.log(date.toLocaleString("pt-BR"))
+// Exibe data e hora em styles diferentes.
+console.log(
+  date.toLocaleString("pt-BR", {
+    dateStyle: "short",
+  })
+)
+
+console.log(
+  date.toLocaleString("pt-BR", {
+    dateStyle: "long",
+  })
+)
+
+console.log(
+  date.toLocaleString("pt-BR", {
+    dateStyle: "medium",
+  })
+)
+
+console.log(
+  date.toLocaleString("pt-BR", {
+    dateStyle: "full",
+  })
+)
+
+console.log(
+  date.toLocaleString("pt-BR", {
+    day: "2-digit",
+    month: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+  })
+)
+
+const amount = 1223.5
+console.log(
+  amount.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  })
+)
