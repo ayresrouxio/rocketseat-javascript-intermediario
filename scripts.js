288f20c12fd4aa10ed3a664a3159476cc130d546
Ayres Roux <ayresrouxio@gmail.com>
2024-12-17 07:30:37 -0300

    aula: convertendo uma data para string

--- scripts.js
+++ scripts.js
@@ -1,15 +1,10 @@
 let date = new Date("2024-07-02T14:30:00")
 
-// Formata para o dia sempre ter 2 dígitos.
-let day = date.getDate().toString().padStart(2, "0")
-console.log(day)
+// Simplesmente converte para String.
+console.log(date.toString())
 
-// Formata para o mês sempre ter 2 dígitos.
-let month = (date.getMonth() + 1).toString().padStart(2, "0")
-console.log(month)
+// Retorna somente a data.
+console.log(date.toDateString())
 
-let year = date.getFullYear()
-let hours = date.getHours()
-let minutes = date.getMinutes()
-
-console.log(`${day}/${month}/${year} às ${hours}:${minutes}`)
+// Retorna somente a hora.
+console.log(date.toTimeString())
