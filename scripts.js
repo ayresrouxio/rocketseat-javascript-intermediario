2a93c631d836a0c787648a203b603037fa799d4d
Ayres Roux <ayresrouxio@gmail.com>
2024-12-16 22:56:46 -0300

    aula: verificando se existe um conteúdo no array

--- scripts.js
+++ scripts.js
@@ -1,27 +1,5 @@
-let myArray = [
-  "Um texto",
-  10,
-  true,
-  function () {
-    console.log("Função dentro do array")
-  },
-  {
-    name: "Rodrigo",
-    email: "rodrigo@email.com",
-  },
-]
+let fruits = ["Apple", "Orange", "Banana"]
 
-// Texto.
-console.log(myArray[0])
-
-// Número.
-console.log(myArray[1])
-
-// Boolean.
-console.log(myArray[2])
-
-// Função.
-myArray[3]()
-
-// Objeto.
-console.log(myArray[4].name)
+// Verifica se um item existe no Array.
+console.log(fruits.includes("Apple"))
+console.log(fruits.includes("Strawberry"))
