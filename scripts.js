af0001f48575478cbf5f54a6f060021731cd9b56
Ayres Roux <ayresrouxio@gmail.com>
2024-12-17 06:54:20 -0300

    aula: continue

--- scripts.js
+++ scripts.js
@@ -1,26 +1,8 @@
-// Break encerra a execução da repetição ou switch para seguir para próxima instrução.
+// Continue encerra (pula) a execução das instruções na iteração atual e continua a execução do loop com a próxima iteração.
 
-let option = 3
-
-switch (option) {
-  case 1:
-    console.log("Cadastrar")
-    break
-  case 2:
-    console.log("Editar")
-    break
-  case 3:
-    console.log("Excluir")
-    break
-  default:
-    console.log("Opção inválida")
-    break
-}
-
-// Utilizando o break para finalizar a repetição.
 for (let i = 0; i < 10; i++) {
   if (i === 5) {
-    break
+    continue
   }
 
   console.log(i)
